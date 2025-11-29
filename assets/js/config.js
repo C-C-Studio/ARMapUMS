6785eeb88276c96eb24f07f9ab68958f4d22d106
Rizalimaru <113398108+Rizalimaru@users.noreply.github.com>
2025-11-29 09:31:08 +0700

    Secret

---
+++ assets/js/config.js
@@ -0,0 +1,11 @@
+// nanti ganti pake akun kampus/cnc, ini pake akun ku (AprilArn) dawg
+// note: taruh file ini di 'assets/js/config.js(file ini)'
+
+// 1. https://www.maptiler.com/
+const MAPTILER_API_KEY = 'FynxEs1bILoP78wso2ZW'; 
+
+// 2. https://www.mapbox.com/
+const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiYXByaWxhcm4iLCJhIjoiY21oazN5emtvMWs4bTJqcHlmenpxbXE3ZSJ9.VXLNeQB60q9Xf8lj3L1PCg';
+
+// 3. Google Maps API Key
+const GOOGLE_MAPS_API_KEY = 'AIzaSyA2CtbpGMMhj1-Kg5WWRZwsbCFS1u5TdCU';
